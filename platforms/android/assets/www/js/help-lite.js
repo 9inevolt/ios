fce7e0a38ce06d468e28f477fdd42dec0b077223
9inevolt <9inevolt@twitch.tv>
2014-08-26 21:42:23 -0700

    dynamically load all css and js in proper order with correct absolute urls

--- platforms/android/assets/www/js/help-lite.js
+++ platforms/android/assets/www/js/help-lite.js
@@ -1,4 +1,66 @@
 ;(function($, window, document, undefined) {
+  // RegExp that matches opening and closing browser-stripped tags.
+  // $1 = slash, $2 = tag name, $3 = attributes
+  var matchTag = /<(\/?)(html|head|body|title|base|meta)(\s+[^>]*)?>/ig;
+  // Unique id prefix for selecting placeholder elements.
+  var prefix = 'hd' + +new Date;
+  // A node under which a temporary DOM tree can be constructed.
+  var parent;
+ 
+  $.htmlDoc = function(html) {
+    // A collection of "intended" elements that can't be rendered cross-browser
+    // with .innerHTML, for which placeholders must be swapped.
+    var elems = $();
+    // Input HTML string, parsed to include placeholder DIVs. Replace HTML,
+    // HEAD, BODY tags with DIV placeholders.
+    var htmlParsed = html.replace(matchTag, function(tag, slash, name, attrs) {
+      // Temporary object in which to hold attributes.
+      var obj = {};
+      // If this is an opening tag...
+      if ( !slash ) {
+        // Add an element of this name into the collection of elements. Note
+        // that if a string of attributes is added at this point, it fails.
+        elems = elems.add('<' + name + '/>');
+        // If the original tag had attributes, create a temporary div with
+        // those attributes. Then, copy each attribute from the temporary div
+        // over to the temporary object.
+        if ( attrs ) {
+          $.each($('<div' + attrs + '/>')[0].attributes, function(i, attr) {
+            obj[attr.name] = attr.value;
+          });
+        }
+        // Set the attributes of the intended object based on the attributes
+        // copied in the previous step.
+        elems.eq(-1).attr(obj);
+      }
+      // A placeholder div with a unique id replaces the intended element's
+      // tag in the parsed HTML string.
+      return '<' + slash + 'div'
+        + (slash ? '' : ' id="' + prefix + (elems.length - 1) + '"') + '>';
+    });
+ 
+    // If no placeholder elements were necessary, just return normal
+    // jQuery-parsed HTML.
+    if ( !elems.length ) {
+      return $(html);
+    }
+    // Create parent node if it hasn't been created yet.
+    if ( !parent ) {
+      parent = $('<div/>');
+    }
+    // Create the parent node and append the parsed, place-held HTML.
+    parent.html(htmlParsed);
+    // Replace each placeholder element with its intended element.
+    $.each(elems, function(i) {
+      var elem = parent.find('#' + prefix + i).before(elems[i]);
+      elems.eq(i).html(elem.contents());
+      elem.remove();
+    });
+    // Return the topmost intended element(s), sans text nodes, while removing
+    // them from the parent element with unwrap.
+    return parent.children().unwrap();
+  };
+
         var baseUrl = "http://www.destiny.gg";
 
 	var urlParseRE = /^\s*(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
@@ -138,43 +200,63 @@
 		return ele;
 	};
 	
-	var _parse = function(html) {
+	var _parseBody = function(html) {
 		var page = $(html.split( /<\/?body[^>]*>/gmi )[1] || "" )
 		return page;
+	};
+
+	var _fixLink = function(_, elem) {
+	    var href = $(elem).attr("href");
+		if ( href.search( "#" ) !== -1 ) {
+		    href = href.replace( /[^#]*#/, "" );
+		    if ( !href ) {
+			//link was an empty hash meant purely
+			//for interaction, so we ignore it.
+			return;
+		    } else if ( !isPath( href ) ) {
+			//we have a simple id so use the documentUrl as its base.
+			href = makeUrlAbsolute( "#" + href, baseUrl );
+		    }
+		}
+
+		if ( isPath( href ) ) {
+		    //we have a path so make it the href we want to load.
+		    href = makeUrlAbsolute( href, baseUrl );
+		}
+
+		$(elem).attr("href", href);
+		//console.debug("rewrite: " + href);
 	};
 	
 	var _loadSuccess = function(data, textStatus, jqXHR) {
 		console.debug("loadSuccess");
-		var content = $(_parse(data));
-		content.find("script[src]").detach();
-		//console.debug(content.html());
+		var allContent = $.htmlDoc(data);
+		var content = _parseBody(data).not("script").find("script").remove().end();
+		var cssContent = allContent.find("link[href]").addBack("link[href]");
+		var scriptContent = allContent.find("script[src]").addBack("script[src]");
+		var inlineContent = allContent.find("script").addBack("script").not("script[src]");
 		//$(document.body).empty();
 
-                //rewrite relative links to absolute
-                content.find("a[href]").each(function() {
-                    var href = $(this).attr("href");
-                    if ( href.search( "#" ) !== -1 ) {
-                        href = href.replace( /[^#]*#/, "" );
-                        if ( !href ) {
-                            //link was an empty hash meant purely
-                            //for interaction, so we ignore it.
-                            return;
-                        } else if ( !isPath( href ) ) {
-                            //we have a simple id so use the documentUrl as its base.
-                            href = makeUrlAbsolute( "#" + href, baseUrl );
-                        }
-                    }
+		var lazycss = cssContent.each(_fixLink).map(function() { return this.href; }).get();
+		LazyLoad.css(lazycss);
 
-                    if ( isPath( href ) ) {
-                        //we have a path so make it the href we want to load.
-                        href = makeUrlAbsolute( href, baseUrl );
-                    }
-
-                    $(this).attr("href", href);
-                    //console.debug("rewrite: " + href);
-                });
-
+		content.find("a[href],link[href]").addBack("a[href],link[href]").each(_fixLink);
 		content.prependTo(document.body);
+
+		var lazyjs = [];
+		scriptContent.each(function() {
+		    var src = $(this).attr("src");
+                    if ( isPath( src ) ) {
+                        src = makeUrlAbsolute( src, baseUrl );
+                    }
+
+		    //console.debug("inject: " + src);
+		    lazyjs.push(src);
+		});
+
+		LazyLoad.js(lazyjs, function() {
+		    inlineContent.appendTo(document.body);
+		});
 	};
 	
 	var _loadError = function(jqXHR, textStatus, errorThrown) {
@@ -195,16 +277,16 @@
 			error: _loadError
 		});
 	};
+
+	// phonegap ready
+	var pgReady = $.Deferred();
+	document.addEventListener("deviceready", pgReady.resolve, false);
+	$.when(pgReady).then(function () {
+	    console.debug("pgReady");
+	    _loadChat();
+	});
 	
 	$(document).ready(function() {
-		// phonegap ready
-		var pgReady = $.Deferred();
-		document.addEventListener("deviceready", pgReady.resolve, false);
-		$.when(pgReady).then(function () {
-		    console.debug("allready");
-		    _loadChat();
-		});
-
 		// click routing - direct to HTTP or Ajax, accordingly
 		$(document).bind( "click", function( event ) {
 			if ( event.isDefaultPrevented() ) {
@@ -252,3 +334,4 @@
 		});
 	});
 })(jQuery, window, document);
+jQuery.noConflict();
